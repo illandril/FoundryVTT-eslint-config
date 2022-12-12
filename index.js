149b0f4adfde693429bfbf79d98d50f54568520b
Joe Spandrusyszyn <github@illandril.net>
2022-12-11 19:03:43 -0500

    Reduce typescript strictness

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ module.exports = {
     'plugin:import/typescript',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:@typescript-eslint/strict',
   ],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
